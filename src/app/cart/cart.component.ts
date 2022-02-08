5424653e695bbd89ce755d428c2c04ab797145b2
stefan-schwarz <mail.stefanschwarz@web.de>
2022-02-08 13:58:24 +0100

    done using forms

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service'
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,21 @@ export class CartComponent {
 
   items = this.cartService.getItems();
   
+  checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
     ) { }
     
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.log('Your order has been submitted', this.checkoutForm.value);
+    window.alert('Your order has been submitted');
+    this.checkoutForm.reset;
+  }
+
 }
